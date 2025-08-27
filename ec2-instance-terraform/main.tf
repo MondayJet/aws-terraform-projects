30da5c7fbba95bed9fec569e3fec7a7bd6ad76a9
MondayJet <john.ephraim4christ@gmail.com>
2025-08-27 03:48:08 +0100

    ec2 instance deployment file

---
+++ ec2-instance-terraform/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "hello_world" {
+    ami = data.aws_ami.ubuntu.id
+    subnet_id = data.aws_subnets.default.ids[0]
+    instance_type = var.instance_type
+
+    tags = {
+      Name = "MyVm"
+    
+    }
+
+}
+
+
---
+++ ec2-instance-terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {
+  type = string
+  description = "The type of instance to launch."
+  default = "t2.micro"
+}
+
