b77fc83afbf110a6bd525a06debcf849e01c58a8
MondayJet <john.ephraim4christ@gmail.com>
2025-09-05 13:43:11 +0100

    Updated date for seemless instance deploy

--- ec2-instance-terraform/main.tf
+++ ec2-instance-terraform/main.tf
@@ -3,11 +3,29 @@ resource "aws_instance" "hello_world" {
     subnet_id = data.aws_subnets.default.ids[0]
     instance_type = var.instance_type
 
+
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
     tags = {
-      Name = "MyVm"
-    
+      Name = "jenkins"
     }
 
+
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+
+  ingress{
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
+
 
 
--- ec2-instance-terraform/variables.tf
+++ ec2-instance-terraform/variables.tf
@@ -4,3 +4,10 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+variable "server_port" {
+  description = "The port whence the instance can be accessed"
+
+  type = number
+
+  default = 22
+}
